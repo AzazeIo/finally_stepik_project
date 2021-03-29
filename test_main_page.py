dec394155c6b7a705860b86317540dea46e3294b
GOLOVKINA USTINYA <sakura.ustinya@yandex.ru>
2021-03-30 00:11:18 +0300

    edit assert for login_url

--- test_main_page.py
+++ test_main_page.py
@@ -17,21 +17,21 @@ class TestLoginMainPage():
 
 
 def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
 
 
 def test_should_be_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
 
 def test_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
