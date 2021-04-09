22cec11a407560f7a9b9260b5ad09855148a25b9
GOLOVKINA USTINYA <sakura.ustinya@yandex.ru>
2021-04-10 01:54:05 +0300

    clear main page,edit base page and create tests for login link

--- test_product_page.py
+++ test_product_page.py
@@ -42,3 +42,15 @@ class TestProductPage():
         page.open()
         page.should_be_add_product_at_the_basket()
         page.should_dissapear_of_success_message()
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
